5d6327ecae88ad6a9cfb621f7554ca2e1b0b7952
JacobKensler <jacobkensler@gmail.com>
2026-03-15 02:02:11 -0400

    idk

--- src/app/(pages)/(landing)/dashboard/page.tsx
+++ src/app/(pages)/(landing)/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import ProfileBadge from "./profile-badge";
-import TranscriptUpload from "./Transcriptupload";
+import TranscriptUpload from "./TranscriptUpload";
 
 export default async function Dashboard() {
   const session = await auth.api.getSession({
